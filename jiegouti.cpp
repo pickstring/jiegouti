889211cb28d031f38d6ec74b1381588783316640
FL <FL@DESKTOP-08Q858E>
2022-07-15 00:28:42 +0800

    jiegouti

---
+++ jiegouti.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+using namespace std;
+
+//语法：struct 结构体名 {结构体成员列表}
+//通过结构体创建变量的方式三种
+//法1、struct 结构体名 变量名
+//法2、struct 结构体名 变量名 = {成员1值， 成员2值， 。。。}
+//法3、定义结构体时顺便创造变量
+
+struct student {
+	string name;
+	int age;
+	int score;
+};
+
+//法3
+struct student1 {
+	string name;
+	int age;
+	int score;
+} s5;
+
+int main() {
+	//法1
+	struct student s1;
+	s1.name = "张三";
+	s1.age = 20;
+	s1.score = 100;
+	cout << s1.score << endl;
+	//法1.1
+	student s2;
+	s2.name = "张三";
+	s2.age = 20;
+	s2.score = 100;
+	cout << s2.score << endl;
+	//法2
+	struct student s3 = { "张三", 20, 100 };
+	//法2.1
+	student s4 = { "张三", 20, 100 };
+	cout << s3.name << s4.age << endl;
+
+	/// <summary>
+	/// 结构体数组
+	/// 语法：struct 结构体名 数组名[元素个数] = {{}，{}，{}，...};
+	/// </summary>
+	/// <returns></returns>
+	student arr[3] = {
+		{"张三", 20, 100},
+		{"李四", 30, 100},
+		{"王五", 40, 100}
+	};
+	cout << arr[0].name << endl;
+	//for循环访问
+
+	/// <summary>
+	/// 结构体指针
+	/// </summary>
+	/// <returns></returns>
+	student s6 = { "张张", 18, 100 };
+	student* p = &s6;
+	cout << s6.name << endl;
+	cout << p->name;
+	return 0;
+}
